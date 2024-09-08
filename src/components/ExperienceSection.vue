c7a21302d77d3f04c0b678aa65d2cba2cd3c949b
Kreoniz <kreoniz@mail.ru>
2024-09-08 18:52:42 +0600

    refactor: remove temporary array

--- src/components/ExperienceSection.vue
+++ src/components/ExperienceSection.vue
@@ -3,44 +3,6 @@ import ExperienceCard from "@/components/ExperienceCard.vue";
 import { useCVStore } from "@/stores/cv";
 
 const cv = useCVStore();
-const jobList = [
-  {
-    id: 1,
-    companyName: "CodeCanvas",
-    position: "Software Architect",
-    description:
-      "Led the design and implementation of scalable and efficient software architectures for remote development environments, ensuring alignment with business requirements and technical standards.",
-    start_date: "10.09.2022",
-    end_date: "Present",
-  },
-  {
-    id: 2,
-    companyName: "Web Weavers",
-    position: "Backend Developer",
-    description:
-      "Designed and implemented robust server-side applications using Node.js and PHP frameworks, ensuring high performance and responsiveness to requests from the front end.",
-    start_date: "03.05.2021",
-    end_date: "10.09.2022",
-  },
-  {
-    id: 3,
-    companyName: "Awesome Company inc.",
-    position: "Fullstack Developer",
-    description:
-      "Designed and developed end-to-end web applications, implementing both front-end and back-end components using JavaScript frameworks (React, Angular) and Node.js.",
-    start_date: "02.01.2021",
-    end_date: "03.05.2021",
-  },
-  {
-    id: 4,
-    companyName: "Awesome Company inc.",
-    position: "Frontend Developer",
-    description:
-      "Designed and implemented dynamic and responsive user interfaces using modern JavaScript frameworks such as React and Vue.js, enhancing user engagement and experience.",
-    start_date: "01.01.2021",
-    end_date: "02.01.2021",
-  },
-];
 </script>
 
 <template>
